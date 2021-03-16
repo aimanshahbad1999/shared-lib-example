f8ffdef36d50eab010597c8eaeee6fda0a0452ed
aimanshahbad1999 <aiman.shahbad@cuelogic.com>
2021-03-16 05:27:44 -0400

    example1-6

--- vars/example1.groovy
+++ vars/example1.groovy
@@ -1,13 +1,22 @@
 #!/usr/bin/env groovy
 
-def call(String name = "Alice") {
-    node{
-        stage("example1"){
-            println("hey hii example1")
-        }
-    }
-}
+// def call(String name = "Alice") {
+//     node{
+//         stage("example1"){
+//             println("hey hii example1")
+//         }
+//     }
+// }
 
 // def example1(){
 //     println("hey you are in example")
 // }
+
+
+def call(String name = "Alice") {
+    script {
+        sh """
+            echo Hi ${name}
+        """
+    }
+}
