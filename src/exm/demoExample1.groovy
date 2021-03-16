b1785f02b53c6a3edc56d567cc5b2105eccba2bc
aimanshahbad1999 <aiman.shahbad@cuelogic.com>
2021-03-16 07:15:25 -0400

    demo1-1

--- src/exm/demoExample1.groovy
+++ src/exm/demoExample1.groovy
@@ -4,9 +4,10 @@ public class demoExample1{
     def name="Aiman"
     def login(){
         if(name=="Aiman"){
-            return true
+            return true;
         }else{
             return "Login failed"
         }
+        return false;
     }
 }
