dbdb1e340c392d5951fca35d7f3764af51b625f3
aimanshahbad1999 <aiman.shahbad@cuelogic.com>
2021-03-16 07:00:32 -0400

    demo1-6

---
+++ src/exm/demoExample1.groovy
@@ -0,0 +1,12 @@
+package com.exm
+
+public class demoExample1{
+    def name="Aiman"
+    def login(){
+        if(name=="Aiman"){
+            return "hey hii welcome Aiman"
+        }else{
+            return "Login failed"
+        }
+    }
+}
