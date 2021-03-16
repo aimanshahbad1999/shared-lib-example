c2b0304d7848ba066b81c2f2b37ab4b099d122b2
aimanshahbad1999 <aiman.shahbad@cuelogic.com>
2021-03-16 06:28:54 -0400

    demo1-1

---
+++ src/AllExample/demoExample1.groovy
@@ -0,0 +1,10 @@
+package AllExample
+
+public class demoExample1{
+    def name="Aiman"
+    if(name=="Aiman"){
+        println("hey hii welcome Aiman")
+    }else{
+        println("Login failed")
+    }
+}
