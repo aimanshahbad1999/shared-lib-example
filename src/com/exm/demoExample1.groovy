4685dd28ce9fb8d0e34c382782fb33e042d01247
aimanshahbad1999 <aiman.shahbad@cuelogic.com>
2021-03-16 07:49:18 -0400

    demo1-12

--- src/com/exm/demoExample1.groovy
+++ src/com/exm/demoExample1.groovy
@@ -1,12 +1,32 @@
-package exm
+// package exm
 
-public class demoExample1{
+// public class demoExample1{
+//     def name="Aiman"
+//     def login(){
+//         if(name=="Aiman"){
+//             println("hey hii welcome Aiman")
+//         }else{
+//             println("Login failed")
+//         }
+//     }
+// }
+
+
+
+package com.exm
+
+public class demoExample1 {
+
+  def login() {
     def name="Aiman"
-    def login(){
-        if(name=="Aiman"){
-            println("hey hii welcome Aiman")
-        }else{
-            println("Login failed")
-        }
+    if (name=="Aiman"){
+      return true;
     }
+    else {
+      println "login failed"
+    }
+    
+    return false;
+  }
+
 }
